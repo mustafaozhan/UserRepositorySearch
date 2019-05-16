c063cfc33fe29b0251fc60699ab7540098765422
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-05-16 18:20:28 +0200

    Project Organised

---
+++ UserRepositorySearch/Main/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  UserRepositorySearch
+//
+//  Created by Mustafa Ozhan on 15/05/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import Foundation
+
+class Repository {
+    let repoName: String = ""
+    let repoUrl: String = ""
+}
---
+++ UserRepositorySearch/Main/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  UserRepositorySearch
+//
+//  Created by Mustafa Ozhan on 15/05/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+
