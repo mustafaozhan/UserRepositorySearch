ca9459f800a8984b8717127250ed1ba12e382672
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-05-16 19:25:51 +0200

    Project is done

--- UserRepositorySearch/Main/ViewController.swift
+++ UserRepositorySearch/Main/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchBar : UISearchBar { return searchController.searchBar }
+    
+    var viewModel = ViewModel()
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureSearchController()
+        
+        viewModel.data
+            .drive(tableView.rx.items(cellIdentifier: "Cell")) { _ , repository, cell in
+                cell.textLabel?.text = repository.repoName
+                cell.detailTextLabel?.text = repository.repoUrl
+            }.disposed(by: disposeBag)
+        
+        searchBar.rx.text.orEmpty
+            .bind(to: viewModel.searchText)
+            .disposed(by: disposeBag)
+        
+        viewModel.data.asDriver()
+            .map { "\($0.count) Repositories" }
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
     }
-
-
+    
+    
+    func configureSearchController() {
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchBar.showsCancelButton = true
+        searchBar.text = "mustafaozhan"
+        searchBar.placeholder = "Enter GitHub Username"
+        tableView.tableHeaderView = searchController.searchBar
+        definesPresentationContext = true
+    }
 }
 
